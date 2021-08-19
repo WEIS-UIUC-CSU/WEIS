64e89af6983729a8c05026214471afd07423975f
Yong Hoon Lee <yonghoonlee@outlook.com>
2021-08-19 10:34:33 -0500

    Direct access to WEIS driver

--- examples/99_level3ccd/script_create_turbine.py
+++ examples/99_level3ccd/script_create_turbine.py
@@ -1,4 +1,5 @@
 from wisdem.commonse.mpi_tools  import MPI
+from warnings import warn
 import os, time
 import weis.inputs as sch
 import numpy as np
@@ -12,11 +13,23 @@ def visualize_tower(wt):
     tower_z_values = wt["components"]["tower"]["outer_shape_bem"]["reference_axis"]["z"]["values"]
     tower_outer_diameter_grid = wt['components']['tower']['outer_shape_bem']['outer_diameter']['grid']
     tower_outer_diameter_values = wt['components']['tower']['outer_shape_bem']['outer_diameter']['values']
+    tower_thickness_grid = wt["components"]["tower"]['internal_structure_2d_fem']['layers'][0]['thickness']['grid']
+    tower_thickness_values = wt["components"]["tower"]['internal_structure_2d_fem']['layers'][0]['thickness']['values']
+    if (
+            np.allclose(tower_z_grid, tower_outer_diameter_grid) and
+            np.allclose(tower_z_grid, tower_thickness_grid)
+    ):
+        print('tower grid consistency test passed')
+    else:
+        warn('tower grid consistency test failed')
     print('rotor diameter = {:}'.format(rotor_diameter))
     print('hub height = {:}'.format(hub_height))
     print('tower height = {:}'.format(tower_z_values[-1]))
     print('tower outer diameter at bottom = {:}'.format(tower_outer_diameter_values[0]))
     print('tower outer diameter at top = {:}'.format(tower_outer_diameter_values[-1]))
+    print('tower thickness at bottom = {:}'.format(tower_thickness_values[0]))
+    print('tower thickness at top = {:}'.format(tower_thickness_values[-3]))
+    print('tower thickness at yaw control device = {:}'.format(tower_thickness_values[-1]))
 
 run_dir = os.path.dirname( os.path.realpath(__file__) ) + os.sep
 wisdem_examples = os.path.join(os.path.dirname( os.path.dirname( os.path.dirname( os.path.realpath(__file__) ) ) ), "WISDEM", "examples")
@@ -31,17 +44,24 @@ else:
 
 tt = time.time()
 
-p_tower_div = 12
+# Parameters
+p_tower_div = 11
 p_tower_bottom_height = 15.0
 p_tower_bottom_diameter = 10.0
-d_hub_height = 170.0
+p_tower_top_thickness = 0.023998
+
+# Design variables
+d_hub_height = 150.0
 d_tower_top_diameter = 6.5
-d_rotor_diameter = 242.25
+d_tower_bottom_thickness = 0.041058
+d_tower_top_thickness = 0.020826
+d_rotor_diameter = 240
 
 wt = sch.load_geometry_yaml(fname_wt_input)
 tower_grd = wt["components"]["tower"]["outer_shape_bem"]["reference_axis"]["z"]["grid"]
 tower_val = wt["components"]["tower"]["outer_shape_bem"]["reference_axis"]["z"]["values"]
 tower_dia = wt['components']['tower']['outer_shape_bem']['outer_diameter']['values']
+tower_thk = wt["components"]["tower"]['internal_structure_2d_fem']['layers'][0]['thickness']['values']
 hub_val = wt['assembly']['hub_height']
 tower_top_to_hub_height = hub_val - tower_val[-1]
 tower_top_height = d_hub_height - tower_top_to_hub_height
@@ -72,6 +92,19 @@ for tidx in range(4, tower_diameter_new.shape[0]):
     elif np.remainder(tidx, 2) == 1:
         fidx = int((tidx-1)/2) - 1
         tower_diameter_new[tidx] = tower_diameter_fill[fidx]
+tower_thk = np.array(tower_thk)
+tower_thk = (tower_thk - tower_thk[0])/(tower_thk[-3] - tower_thk[0])
+tower_thk = d_tower_bottom_thickness + (d_tower_top_thickness - d_tower_bottom_thickness)*tower_thk
+tower_thickness_interp1d = interp1d(
+    np.array([tower_grd[i] for i in range(0,len(tower_grd)-1,2)])/tower_grd[len(tower_grd)-3],
+    [tower_thk[i] for i in range(0,len(tower_grd)-1,2)]
+)
+tower_thickness_new = d_tower_bottom_thickness*np.ones(tower_zcoord_new.shape, dtype=float)
+for tidx in range(1, tower_thickness_new.shape[0]-2, 2):
+    tower_thickness_new[tidx + 1] = tower_thickness_interp1d(tower_grd_new[tidx + 1]/tower_grd_new[len(tower_grd_new)-3])
+    tower_thickness_new[tidx] = tower_thickness_new[tidx + 1]
+tower_thickness_new[tower_thickness_new.shape[0]-2] = p_tower_top_thickness
+tower_thickness_new[tower_thickness_new.shape[0]-1] = p_tower_top_thickness
 
 wt['assembly']['rotor_diameter'] = d_rotor_diameter
 wt['assembly']['hub_height'] = d_hub_height
@@ -79,6 +112,10 @@ wt["components"]["tower"]["outer_shape_bem"]["reference_axis"]["z"]["grid"] = to
 wt["components"]["tower"]["outer_shape_bem"]["reference_axis"]["z"]["values"] = tower_zcoord_new.tolist()
 wt['components']['tower']['outer_shape_bem']['outer_diameter']['grid'] = tower_grd_new.tolist()
 wt['components']['tower']['outer_shape_bem']['outer_diameter']['values'] = tower_diameter_new.tolist()
+wt["components"]["tower"]['internal_structure_2d_fem']['reference_axis']['z']['grid'] = tower_grd_new.tolist()
+wt["components"]["tower"]['internal_structure_2d_fem']['reference_axis']['z']['values'] = tower_zcoord_new.tolist()
+wt["components"]["tower"]['internal_structure_2d_fem']['layers'][0]['thickness']['grid'] = tower_grd_new.tolist()
+wt["components"]["tower"]['internal_structure_2d_fem']['layers'][0]['thickness']['values'] = tower_thickness_new.tolist()
 
 sch.write_geometry_yaml(wt, 'test_turbine.yaml')
 
