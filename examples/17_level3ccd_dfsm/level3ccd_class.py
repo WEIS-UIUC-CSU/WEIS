d3127f5ad6f4921615f03fb3a014e2b5850a8a09
Yong Hoon Lee <yonghoonlee@outlook.com>
2022-06-10 23:00:20 -0500

    CMA-ES minimizer added

--- examples/17_level3ccd_dfsm/level3ccd_class.py
+++ examples/17_level3ccd_dfsm/level3ccd_class.py
@@ -4,6 +4,7 @@ import openmdao.api as om
 import weis
 import sqlite3
 import yaml
+import time
 
 from weis.glue_code.gc_LoadInputs           import WindTurbineOntologyPythonWEIS
 from wisdem.glue_code.gc_WT_InitModel       import yaml2openmdao
@@ -1513,6 +1514,115 @@ class optimization_problem:
             fplus = approx_fprime(xplus, self.objective, self.num_diff_eps)
             hess[idx, :] = (fplus - fzero)/h
         return hess[row, col]
+
+
+def cmaes_minimizer(lambda_pop, num_iteration, N, x0_scaled, Cost_calculator, num_workers, debug=False):
+    xmean = x0_scaled  # mean value of initial population
+    sigma = 0.3;       # coordinate wise standard deviation (step size)
+    stopeval = lambda_pop*num_iteration
+    
+    mu = lambda_pop/2      # number of parents/points for recombination
+    weights = np.log(mu+1/2) - np.log(np.arange(mu+1)[1:]) # muXone array for weighted recombination
+    mu = np.floor(mu)        
+    weights = weights/np.sum(weights)     #normalize recombination weights array
+    mueff = np.sum(weights)**2/np.sum(weights**2) # variance-effectiveness of sum w_i x_i
+    
+    # Strategy parameter setting: Adaptation
+    cc = (4+mueff/N) / (N+4 + 2*mueff/N)            # time constant for cumulation for C
+    cs = (mueff+2) / (N+mueff+5)                    # t-const for cumulation for sigma control
+    c1 = 2 / ((N+1.3)**2+mueff)                     # learning rate for rank-one update of C
+    cmu = min(1-c1, 2 * (mueff-2+1/mueff) / ((N+2)**2+mueff))               # and for rank-mu update
+    damps = 1 + 2*np.max(np.array([0, np.sqrt((mueff-1)/(N+1))-1])) + cs    # damping for sigma, usually close to 1
+    # Initialize dynamic (internal) strategy parameters and constants
+    pc = np.zeros((N,1))        # evolution paths for C
+    ps = np.zeros((N,1))        # evolution paths for sigma
+    B = np.eye(N,N)             # B defines the coordinate system
+    D = np.ones((N,1))          # diagonal D defines the scaling
+    if N != 1:
+        C = B @ np.diag(D.squeeze()**2) @ B.T           # covariance matrix C
+        invsqrtC = B @ np.diag(D.squeeze()**-1) @ B.T   # C^-1/2 
+    else:
+        C = (B @ (D**2) @ B.T)
+        invsqrtC = B @ (D**-1) @ B.T    # C^-1/2 
+    
+    eigeneval = 0;                      # track update of B and D
+    chiN=N**0.5*(1-1/(4*N)+1/(21*N**2)) # expectation of ||N(0,I)|| == norm(randn(N,1))                      
+                                        
+    # -------------------- Generation Loop --------------------------------
+    counteval = 0
+    iteration_index = 0   
+
+    x_opt_vector = np.zeros((N,num_iteration))
+    f_opt_vector = np.zeros(num_iteration)
+    x_total_vec = np.zeros((N,num_iteration*lambda_pop))
+    f_total_vec = np.zeros(num_iteration*lambda_pop)
+
+    while counteval < stopeval:
+        arx = np.zeros((N,lambda_pop))
+        for k in np.arange(lambda_pop):
+            arx[:,k] = xmean.squeeze() + (sigma * B @ (D * np.random.randn(N,1))).squeeze() # m + sig * Normal(0,C) 
+
+        t = time.time()
+        arfitness,arx = Cost_calculator(arx,num_workers)
+        counteval += lambda_pop
+            
+        arindex = arfitness.argsort() 
+        arfitness.sort()
+        xold = xmean
+        xmean = arx[:,arindex[np.arange(int(mu))]] @ np.reshape(weights,(-1,1))   # recombination, new mean value
+        
+        # Cumulation: Update evolution paths
+        ps = (1-cs)*ps+np.sqrt(cs*(2-cs)*mueff) * invsqrtC @ (xmean-xold) / sigma 
+        hsig = np.linalg.norm(ps,2)/np.sqrt(1-(1-cs)**(2*counteval/lambda_pop))/chiN < 1.4 + 2/(N+1);
+        pc = (1-cc)*pc+ hsig * np.sqrt(cc*(2-cc)*mueff) * (xmean-xold) / sigma;
+ 
+        # Adapt covariance matrix C
+        artmp = (1/sigma) * (arx[:,arindex[np.arange(int(mu))]]-np.tile(xold,(1,int(mu))))
+        C = (1-c1-cmu) * C + c1 * (pc*pc.T + (1-hsig) * cc*(2-cc) * C)+ cmu * artmp @ np.diag(weights) @ artmp.T; # plus rank mu update
+
+        # Adapt step size sigma
+        sigma = sigma * np.exp((cs/damps)*(np.linalg.norm(ps,2)/chiN - 1)); 
+    
+        # Decomposition of C into B*diag(D.^2)*B' (diagonalization)
+        if counteval - eigeneval > lambda_pop/(c1+cmu)/N/10:  # to achieve O(N^2):
+            eigeneval = counteval
+            C = np.triu(C) + np.triu(C,1).T; # enforce symmetry
+            D, B = la.eig(C)                # eigen decomposition, B==normalized eigenvectors
+            D = D.real
+            D = np.reshape(D,(-1,1))
+            D = np.sqrt(D);        # D is a vector of standard deviations now
+            if N != 1:
+                invsqrtC = B @ np.diag(D.squeeze()**-1) @ B.T
+            else:
+                invsqrtC = B @ (D**-1) @ B.T
+            
+        x_opt_vector[:,iteration_index] = arx[:, arindex[0]]
+        f_opt_vector[iteration_index] = arfitness[0]
+
+        x_total_vec[:,lambda_pop*iteration_index:lambda_pop*(iteration_index+1)] = arx
+        f_total_vec[lambda_pop*iteration_index:lambda_pop*(iteration_index+1)] = arfitness
+
+        iteration_index = iteration_index + 1
+
+        if debug:
+            with open('Report_CMAES.txt','w') as f:
+                f.write(f'each iteration time: {int(time.time()-t)/60}')
+                f.write('\n')
+                f.write(f'iteration: {int(counteval/lambda_pop)}')
+                f.write('\n')
+                f.write(f'x_opt_vector: {x_opt_vector}')
+                f.write('\n')
+                f.write(f'f_opt_vector: {f_opt_vector}')
+                f.write('\n')
+                f.write(f'x_total_vec: {x_total_vec}')
+                f.write('\n')
+                f.write(f'f_total_vec: {f_total_vec}')
+
+    xmin = arx[:, arindex[0]] # Return best point of last iteration.
+                              # Notice that xmean is expected to be even better.
+    fmin=arfitness[0]
+    return xmin,fmin,x_opt_vector,f_opt_vector,x_total_vec,f_total_vec
+
 
 
 if __name__ == '__main__':
